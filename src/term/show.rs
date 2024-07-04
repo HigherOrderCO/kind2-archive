8541573426d546894b71becc2dc796bb2d20924e
Enrico Zandomeni Borba <enricozb@gmail.com>
2024-07-04 10:19:22 +0200

    Display for Term and Oper

--- src/term/show.rs
+++ src/term/show.rs
@@ -1,263 +1,193 @@
-use crate::{*};
+use std::fmt::Display;
+
+use crate::*;
 
 impl Oper {
-
   pub fn show(&self) -> String {
-    return self.format().flatten(None);
+    self.to_string()
   }
-
-  pub fn format(&self) -> Box<Show> {
-    Show::text(match self {
-      Oper::Add => "+",
-      Oper::Sub => "-",
-      Oper::Mul => "*",
-      Oper::Div => "/",
-      Oper::Mod => "%",
-      Oper::Eq  => "==",
-      Oper::Ne  => "!=",
-      Oper::Lt  => "<",
-      Oper::Gt  => ">",
-      Oper::Lte => "<=",
-      Oper::Gte => ">=",
-      Oper::And => "&",
-      Oper::Or  => "|",
-      Oper::Xor => "^",
-      Oper::Lsh => "<<",
-      Oper::Rsh => ">>",
-    })
-  }
-
 }
 
 impl Term {
-
   pub fn show(&self) -> String {
-    return self.format().flatten(None);
+    self.to_string()
   }
+}
 
-  pub fn format(&self) -> Box<Show> {
-    return self.clean().format_go();
+impl Display for Oper {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    write!(
+      f,
+      "{}",
+      match self {
+        Oper::Add => "+",
+        Oper::Sub => "-",
+        Oper::Mul => "*",
+        Oper::Div => "/",
+        Oper::Mod => "%",
+        Oper::Eq  => "==",
+        Oper::Ne  => "!=",
+        Oper::Lt  => "<",
+        Oper::Gt  => ">",
+        Oper::Lte => "<=",
+        Oper::Gte => ">=",
+        Oper::And => "&",
+        Oper::Or  => "|",
+        Oper::Xor => "^",
+        Oper::Lsh => "<<",
+        Oper::Rsh => ">>",
+      }
+    )
   }
+}
 
-  pub fn format_go(&self) -> Box<Show> {
-
+impl Display for Term {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
     // Shows a Nat
     if let Some(nat) = self.as_nat() {
-      return Show::text(&format!("{}", nat));
+      return write!(f, "{nat}");
     }
 
     // Shows a List
     if let Some(list) = self.as_list() {
-      return list.format();
+      return write!(f, "{list}");
     }
 
     // Shows an Equal
     if let Some(equal) = self.as_equal() {
-      return equal.format();
+      return write!(f, "{equal}");
     }
 
     // Shows an ADT
     if let Some(adt) = self.as_adt() {
-      return adt.format();
+      return write!(f, "{adt}");
     }
 
     match self {
       Term::All { .. } => {
-        let mut bnd = vec![];
         let mut bod = self;
-        while let Term::All { era: _, nam, inp, bod: in_bod } = bod {
-          bnd.push(Show::call("", vec![
-            Show::glue("", vec![
-              Show::text("∀("),
-              Show::text(nam),
-              Show::text(": "),
-            ]),
-            inp.format_go(),
-            Show::text(")"),
-          ]));
-          bod = in_bod;
+        while let Term::All {
+          era: _,
+          nam,
+          inp,
+          bod: in_bod,
+        } = bod
+        {
+          write!(f, "∀({nam}: {inp}) ")?;
         }
-        Show::pile(" ", vec![
-          Show::pile(" ", bnd),
-          bod.format_go(),
-        ])
-      },
+
+        write!(f, "{bod}")
+      }
+
       Term::Lam { .. } => {
-        let mut bnd = vec![];
         let mut bod = self;
-        while let Term::Lam { era: _, nam, bod: in_bod } = bod {
-          bnd.push(Show::text(&format!("λ{}",nam)));
+        while let Term::Lam {
+          era: _,
+          nam,
+          bod: in_bod,
+        } = bod
+        {
+          write!(f, "λ{nam} ");
           bod = in_bod;
         }
-        Show::pile(" ", vec![
-          Show::glue(" ", bnd),
-          bod.format_go(),
-        ])
-      },
+
+        write!(f, "{bod}")
+      }
+
       Term::App { .. } => {
         let mut fun = self;
-        let mut spn = vec![];
-        while let Term::App { era: _, fun: in_fun, arg } = fun {
-          spn.push(arg);
+        let mut args = vec![];
+        while let Term::App {
+          era: _,
+          fun: in_fun,
+          arg,
+        } = fun
+        {
+          args.push(arg.to_string());
           fun = in_fun;
         }
-        let mut vec = vec![];
-        vec.push(Show::glue("", vec![
-          Show::text("("),
-          fun.format_go(),
-        ]));
-        spn.reverse();
-        for arg in spn {
-          vec.push(arg.format_go());
-        }
-        vec.push(Show::text(")"));
-        Show::call(" ", vec)
+        args.reverse();
+
+        write!(f, "({fun} {})", args.join(" "))
       }
+
       Term::Ann { chk: _, val, typ: _ } => {
-        val.format_go()
-        //Show::call(" ", vec![
-          //val.format_go(),
-          //Show::text("::"),
-          //typ.format_go(),
-        //])
-      },
-      Term::Slf { nam, typ, bod } => {
-        Show::pile(" ", vec![
-          Show::glue("", vec![
-            Show::text("$("),
-            Show::text(nam),
-            Show::text(": "),
-            typ.format_go(),
-            Show::text(")"),
-          ]),
-          bod.format_go(),
-        ])
+        write!(f, "{val}")
       }
+
+      Term::Slf { nam, typ, bod } => {
+        write!(f, "$({nam}: {typ}) {bod}")
+      }
+
       Term::Ins { val } => {
-        Show::glue("", vec![
-          Show::text("~"),
-          val.format_go(),
-        ])
-      },
+        write!(f, "~{val}")
+      }
+
       Term::Set => {
-        Show::text("*")
-      },
+        write!(f, "*")
+      }
+
       Term::U60 => {
-        Show::text("U60")
-      },
+        write!(f, "U60")
+      }
+
       Term::Num { val } => {
-        Show::text(&format!("{}", val))
-      },
+        write!(f, "{val}")
+      }
+
       Term::Op2 { opr, fst, snd } => {
-        Show::call(" ", vec![
-          Show::glue("", vec![
-            Show::text("("),
-            opr.format(),
-          ]),
-          Show::glue("", vec![
-            fst.format_go(),
-          ]),
-          Show::glue("", vec![
-            snd.format_go(),
-          ]),
-          Show::text(")"),
-        ])
-      },
+        write!(f, "({opr} {fst} {snd})")
+      }
+
       Term::Swi { nam, x, z, s, p } => {
-        Show::call(" ", vec![
-          Show::glue(" ", vec![
-            Show::text("switch"),
-            Show::text(nam),
-            Show::text("="),
-            x.format_go(),
-            Show::text("{"),
-          ]),
-          Show::glue("", vec![
-            Show::text("0: "),
-            z.format_go(),
-          ]),
-          Show::glue("", vec![
-            Show::text("_: "),
-            s.format_go(),
-            Show::text(" "),
-          ]),
-          Show::glue("", vec![
-            Show::text("}: "),
-            p.format_go(),
-          ]),
-        ])
-      },
+        write!(f, "switch {nam} = {x} {{0: {z} _: {s}}}: {p}")
+      }
+
       Term::Let { nam, val, bod } => {
-        Show::glue("", vec![
-          Show::text("let "),
-          Show::text(nam),
-          Show::text(" = "),
-          Show::inc(),
-          val.format_go(),
-          Show::dec(),
-          Show::semi(),
-          bod.format_go(),
-        ])
-      },
+        write!(f, "let {nam} = {val}; {bod}")
+      }
+
       Term::Use { nam, val, bod } => {
-        Show::glue("", vec![
-          Show::text("use "),
-          Show::text(nam),
-          Show::text(" = "),
-          Show::inc(),
-          val.format_go(),
-          Show::dec(),
-          Show::semi(),
-          bod.format_go(),
-        ])
-      },
+        write!(f, "use {nam} = {val}; {bod}")
+      }
+
       Term::Hol { nam } => {
-        Show::text(&format!("?{}", nam))
-      },
+        write!(f, "?{nam}")
+      }
+
       Term::Met {} => {
-        Show::text("_")
-      },
+        write!(f, "_")
+      }
+
       Term::Var { nam } => {
-        Show::text(nam)
-      },
+        write!(f, "{nam}")
+      }
+
       Term::Src { src: _, val } => {
-        val.format_go()
-      },
+        write!(f, "{val}")
+      }
+
       Term::Txt { txt } => {
-        Show::text(&format!("\"{}\"", txt))
-      },
+        write!(f, "\"{txt}\"")
+      }
+
       Term::Nat { nat } => {
-        Show::text(&format!("{}", nat))
-      },
+        write!(f, "{nat}")
+      }
+
       Term::Mch { mch } => {
-        Show::call(" ", vec![
-          Show::glue(" ", vec![
-            Show::text("match"),
-            Show::text(&mch.name),
-            Show::text("= "),
-            mch.expr.format_go(),
-            Show::text("{"),
-          ]),
-          Show::pile("; ", mch.cses.iter().map(|(nam, bod)| {
-            Show::glue("", vec![
-              Show::text(nam),
-              Show::text(": "),
-              bod.format_go(),
-            ])
-          }).collect()),
-          Show::glue("", vec![
-            Show::text("}"),
-          ]),
-          mch.moti.as_ref().map_or(
-            Show::text(""),
-            |bod| Show::glue("", vec![
-              Show::text(": "),
-              bod.format_go(),
-            ])  
-          ),
-        ])
-      },
+        write!(f, "match {name} = {expr} {{", name = mch.name, expr = mch.expr)?;
+        for (nam, bod) in mch.cses {
+          write!(f, "{nam}: {bod};")?;
+        }
+        write!(f, "}}")?;
+
+        if let Some(moti) = mch.moti {
+          write!(f, ": {moti}")?;
+        }
+
+        Ok(())
+      }
     }
   }
-
 }
